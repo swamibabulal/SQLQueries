0e061f293ed75687440668852b564606edd669f2
Babu Lal Swami <swamibabulal@gmail.com>
2015-01-26 01:23:35 -0800

    Create consecutive zeros create table.sql

---
+++ consecutive zeros create table.sql
@@ -0,0 +1,28 @@
+CREATE TABLE [dbo].[t_assign] (
+    [Id]     NUMERIC (18) NOT NULL,
+    [Name]   VARCHAR (50) NULL,
+    [Status] NUMERIC (18) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(1 AS Decimal(18, 0)), N'aaa', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(2 AS Decimal(18, 0)), N'bbb', CAST(1 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(3 AS Decimal(18, 0)), N'ccc', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(4 AS Decimal(18, 0)), N'ddd', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(5 AS Decimal(18, 0)), N'eee', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(6 AS Decimal(18, 0)), N'fff', CAST(1 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(7 AS Decimal(18, 0)), N'ggg', CAST(2 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(8 AS Decimal(18, 0)), N'hhh', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(9 AS Decimal(18, 0)), N'iii', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(10 AS Decimal(18, 0)), N'jjj', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(11 AS Decimal(18, 0)), N'kkk', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(12 AS Decimal(18, 0)), N'lll', CAST(2 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(13 AS Decimal(18, 0)), N'mmm', CAST(1 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(14 AS Decimal(18, 0)), N'nnn', CAST(1 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(15 AS Decimal(18, 0)), N'ooo', CAST(1 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(16 AS Decimal(18, 0)), N'ppp', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(17 AS Decimal(18, 0)), N'qqq', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(18 AS Decimal(18, 0)), N'q', CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(19 AS Decimal(18, 0)), NULL, CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(20 AS Decimal(18, 0)), NULL, CAST(0 AS Decimal(18, 0)))
+INSERT INTO [dbo].[t_assign] ([Id], [Name], [Status]) VALUES (CAST(21 AS Decimal(18, 0)), NULL, CAST(0 AS Decimal(18, 0)))
